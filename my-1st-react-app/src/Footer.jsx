4e0cc2cc6087d99ca8025e6dcdf30e6bf0813650
anhtri04 <mastercuberskill@gmail.com>
2025-05-23 21:34:56 +0700

    Initial commit

---
+++ my-1st-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>  {new Date().getFullYear()} This is my 1st react website </p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ my-1st-react-app/src/Header.jsx
@@ -0,0 +1,21 @@
+
+
+function Header() {
+    return(
+        <header>
+            <h1>Hello World</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Account</li>
+                    <li>Dashboard</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+
+export default Header
