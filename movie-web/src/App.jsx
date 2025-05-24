4565b991843bae6c8d2cebe9c7098d26783d2c5e
anhtri04 <mastercuberskill@gmail.com>
2025-05-25 00:16:31 +0700

    ini movie web

---
+++ movie-web/src/App.jsx
@@ -0,0 +1,13 @@
+import MovieCard from './components/MovieCard'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <MovieCard movie = {{title:"John Weak 4", release_date:"31/02/2026"}}/>
+    </div>
+  )
+}
+
+export default App
